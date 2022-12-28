e18df8a4c506b52dcb56f63c4681f0b1d9c44ae3
ilulale <56584872+ilulale@users.noreply.github.com>
2022-12-28 23:57:34 +0530

    Update bio.js

--- pages/bio.js
+++ pages/bio.js
@@ -3,9 +3,9 @@ const Bio = () => {
     <div className="mt-3">
       <h3>Kshitij Tapre</h3>
     <p>
-As a full stack developer, I have a strong foundation in computer engineering, specifically with a focus on JavaScript frameworks for both frontend and backend development. My bachelor's degree from Bombay University has given me a solid understanding of the field, and I have applied this knowledge to a number of projects involving Docker and Kubernetes.</p>
+As a full stack developer, I have a strong foundation in computer engineering, specifically with a focus on JavaScript frameworks for both frontend and backend development. My bachelor&apos;s degree from Bombay University has given me a solid understanding of the field, and I have applied this knowledge to a number of projects involving Docker and Kubernetes.</p>
     <p>
-In addition to my technical skills, I am also highly interested in cybersecurity. I am currently exploring options to pursue a master's degree in the field, which would allow me to deepen my understanding of the latest security threats and best practices. My passion for cybersecurity, combined with my technical expertise, makes me well-equipped to tackle a variety of challenges in the field of software development and beyond. I am always looking for new opportunities to learn and grow, and I am excited to bring my skills and expertise to new projects and challenges.
+In addition to my technical skills, I am also highly interested in cybersecurity. I am currently exploring options to pursue a master&apos;s degree in the field, which would allow me to deepen my understanding of the latest security threats and best practices. My passion for cybersecurity, combined with my technical expertise, makes me well-equipped to tackle a variety of challenges in the field of software development and beyond. I am always looking for new opportunities to learn and grow, and I am excited to bring my skills and expertise to new projects and challenges.
     </p>
     </div>
   )
