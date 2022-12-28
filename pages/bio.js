2ba7894ab71fd7a3c23639944787ac2ab222c871
ilulale <xiece181959@students.xavierengg.com>
2022-12-28 18:16:10 +0000

    updated bio

--- pages/bio.js
+++ pages/bio.js
@@ -2,7 +2,11 @@ const Bio = () => {
   return (
     <div className="mt-3">
       <p className="display-4 text-center">I&#39;m Kshitij Tapre</p>
-      <p className="text-center">Full stack developer</p>
+    <p>
+As a full stack developer, I have a strong foundation in computer engineering, specifically with a focus on JavaScript frameworks for both frontend and backend development. My bachelor's degree from Bombay University has given me a solid understanding of the field, and I have applied this knowledge to a number of projects involving Docker and Kubernetes.</p>
+    <p>
+In addition to my technical skills, I am also highly interested in cybersecurity. I am currently exploring options to pursue a master's degree in the field, which would allow me to deepen my understanding of the latest security threats and best practices. My passion for cybersecurity, combined with my technical expertise, makes me well-equipped to tackle a variety of challenges in the field of software development and beyond. I am always looking for new opportunities to learn and grow, and I am excited to bring my skills and expertise to new projects and challenges.
+    </p>
     </div>
   )
 }
