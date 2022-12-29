d397dce55daf2720d58069154132de734f20762d
ilulale <xiece181959@students.xavierengg.com>
2022-12-29 11:53:16 +0000

    styling

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,11 @@ const Home = ({ posts }) => {
     <div className="mt-5">
       {posts.map((post, index) => (
         <Link href={'/blog/' + post.slug} passHref key={index}>
-          <div className="card mb-3 pointer" style={{ maxWidth: '540px', background : '#131316' }}>
+          <div className="card mb-3 w-screen pointer full-screen" style={{ background : '#131316'}}>
             <div className="row g-0">
               <div className="col-md-8">
                 <div className="card-body">
-                  <h5 className="card-title">{post.frontMatter.title}</h5>
+                  <h5 className="card-title mt-2">{post.frontMatter.title}</h5>
                   <p className="card-text">{post.frontMatter.description}</p>
                   <p className="card-text">
                     <small className="text-muted">{post.frontMatter.date}</small><br />
@@ -21,7 +21,7 @@ const Home = ({ posts }) => {
                   </p>
                 </div>
               </div>
-              <div className="col-md-4 m-auto">
+              <div className="col-md-4 m-auto p-2">
                 <Image
                   src={post.frontMatter.thumbnailUrl}
                   className="img-fluid mt-1 rounded-start"
